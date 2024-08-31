6e442b9a3bb71e874688f3bf723854aad9565109
Prankur Tiwari <tiwariprankur61@gmail.com>
2024-08-31 18:16:13 +0530

    basic null and empty check with one fail testcase

--- src/main/java/com/incubyte/incubyte_calculator/SimpleStringCal.java
+++ src/main/java/com/incubyte/incubyte_calculator/SimpleStringCal.java
@@ -3,6 +3,9 @@ package com.incubyte.incubyte_calculator;
 public class SimpleStringCal implements ICalculator {
     @Override
     public int add(String num) {
-        return 0;
+        if (num == null || num.isEmpty()) {
+            return 0;
+        }
+        return 1;
     }
 }
--- src/test/java/com/incubyte/incubyte_calculator/IncubyteCalculatorApplicationTests.java
+++ src/test/java/com/incubyte/incubyte_calculator/IncubyteCalculatorApplicationTests.java
@@ -1,13 +1,36 @@
 package com.incubyte.incubyte_calculator;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class IncubyteCalculatorApplicationTests {
+	private static SimpleStringCal cal;
+
+	@BeforeAll
+	public static void setup() {
+		if (cal == null) {
+			cal = new SimpleStringCal();
+		}
+	}
 
 	@Test
-	void contextLoads() {
+	public void nullCheck() {
+		assertEquals(0, cal.add(null));
+	}
+
+	@Test
+	public void emptyStringCheck() {
+		assertEquals(0, cal.add(""));
+	}
+
+	@Test
+	public void anyNumberCheck() {
+		assertEquals(2, cal.add("2"));
 	}
 
 }
